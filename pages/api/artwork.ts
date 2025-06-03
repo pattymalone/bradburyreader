21b7ed86b1bde0f2f55953f6a40ccfe03601be97
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-02 23:31:05 -0400

    Update artwork.ts

--- pages/api/artwork.ts
+++ pages/api/artwork.ts
@@ -4,6 +4,6 @@ export async function GET() {
   return NextResponse.json({
     title: "Wheat Field with Cypresses",
     artist: "Vincent van Gogh",
-    imageUrl: "https://www.metmuseum.org/-/media/images/art/collection-landing/paintings/1975_1_222.jpg"
+    url: "https://www.metmuseum.org/-/media/images/art/collection-landing/paintings/1975_1_222.jpg"
   })
 }
