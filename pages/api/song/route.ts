982ffa9ebbbcf8d65e69ef9bc638b96f628d250f
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-02 22:11:43 -0400

    Delete pages/api/song directory

--- pages/api/song/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    title: "This Must Be the Place",
-    artist: "Talking Heads",
-    url: "https://open.spotify.com/track/7e89621JPkKaeDSTQ3avtg"
-  })
-}
