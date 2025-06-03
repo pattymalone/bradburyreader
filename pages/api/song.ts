98107e52e7de3a62d5dfa56d4b2135ffcb77299c
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-03 13:22:53 -0400

    Update song.ts

--- pages/api/song.ts
+++ pages/api/song.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from 'next/server'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export async function GET() {
-  return NextResponse.json({
-    title: "This Must Be the Place",
-    artist: "Talking Heads",
-    url: "https://open.spotify.com/track/7e89621JPkKaeDSTQ3avtg"
-  })
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({
+    title: "Clair de Lune",
+    artist: "Claude Debussy",
+    previewUrl: "https://p.scdn.co/mp3-preview/1ebf72d57169cbf90f858df9a7f29fbb0f59c249"
+  });
 }
+
