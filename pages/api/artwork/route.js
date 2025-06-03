d528c11415f87fcfad4488ec2a8a6bb3e62d5a05
pattymalone <114537268+pattymalone@users.noreply.github.com>
2025-06-03 15:08:40 -0400

    Update route.js

--- pages/api/artwork/route.js
+++ pages/api/artwork/route.js
@@ -4,6 +4,6 @@ export async function GET() {
   return NextResponse.json({
     title: "Wheat Field with Cypresses",
     artist: "Vincent van Gogh",
-    url: "https://www.metmuseum.org/-/media/images/art/collection-landing/paintings/1975_1_222.jpg"
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Vincent_van_Gogh_-_Wheat_Field_with_Cypresses_-_Google_Art_Project.jpg/800px-Vincent_van_Gogh_-_Wheat_Field_with_Cypresses_-_Google_Art_Project.jpg"
   });
 }
